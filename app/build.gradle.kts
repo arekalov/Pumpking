47fe775a3cfaf539c65387a0effda14e599c0c5c
arekalov <artyom.rekalov@gmail.com>
2025-06-11 20:46:27 +0300

    [feat] add test workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
     kotlinOptions {
         jvmTarget = "22"
