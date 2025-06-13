625460f4473e82bd963f570cf821be84ddc25496
arekalov <artyom.rekalov@gmail.com>
2025-06-13 10:30:26 +0300

    [feat] add palette

---
+++ app/src/main/java/com/arekalov/pumpking/ui/theme/CompositionLocals.kt
@@ -0,0 +1,7 @@
+package com.arekalov.pumpking.ui.theme
+
+import androidx.compose.runtime.compositionLocalOf
+
+internal val LocalPaletteColors = compositionLocalOf<PumpkingUniversalColors> {
+    error("ProfileLoginPaletteColors is not provided")
+}
--- app/src/main/java/com/arekalov/pumpking/ui/theme/Theme.kt
+++ app/src/main/java/com/arekalov/pumpking/ui/theme/Theme.kt
@@ -1,58 +1,88 @@
 package com.arekalov.pumpking.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val DarkColorScheme: ColorScheme
+    get() = lightColorScheme(
+        primary = darkPrimary,
+        secondary = darkSecondary,
+        onPrimary = darkOnPrimary,
+        onSecondary = darkOnSecondary,
+        surface = darkSurface,
+        surfaceVariant = darkSurfaceVariant,
+        onSurface = darkOnSurface,
+        onSurfaceVariant = darkOnSurfaceVariant,
+        primaryContainer = darkContainer,
+        onPrimaryContainer = darkOnContainer,
+        secondaryContainer = darkContainerVariant,
+        onSecondaryContainer = darkOnContainerVariant,
+    )
+
+private val LightColorScheme: ColorScheme
+    get() = lightColorScheme(
+        primary = lightPrimary,
+        secondary = lightSecondary,
+        onPrimary = lightOnPrimary,
+        onSecondary = lightOnSecondary,
+        surface = lightSurface,
+        surfaceVariant = lightSurfaceVariant,
+        onSurface = lightOnSurface,
+        onSurfaceVariant = lightOnSurfaceVariant,
+        primaryContainer = lightContainer,
+        onPrimaryContainer = lightOnContainer,
+        secondaryContainer = lightContainerVariant,
+        onSecondaryContainer = lightOnContainerVariant,
+    )
+
+internal data class PumpkingUniversalColors(
+    val accentRed: Color,
+    val accentRedVariant: Color,
+    val accentGreen: Color,
+    val accentGreenVariant: Color,
+    val accentBlue: Color,
+    val accentYellow: Color,
+    val accentLightBlue: Color,
+    val accentPurple: Color,
+    val accentOrange: Color,
+    val accentCyan: Color,
+    val accentGrey: Color,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val PumpkingUniversalColorsPalette: PumpkingUniversalColors
+    get() = PumpkingUniversalColors(
+        accentRed = accentRed,
+        accentRedVariant = accentRedVariant,
+        accentGreen = accentGreen,
+        accentGreenVariant = accentGreenVariant,
+        accentBlue = accentBlue,
+        accentYellow = accentYellow,
+        accentLightBlue = accentLightBlue,
+        accentPurple = accentPurple,
+        accentOrange = accentOrange,
+        accentCyan = accentCyan,
+        accentGrey = accentGrey,
+    )
 
 @Composable
 fun PumpkingTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val themeColors = if (isDarkTheme) DarkColorScheme else LightColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(
+        LocalPaletteColors provides PumpkingUniversalColorsPalette
+    ) {
+        MaterialTheme(
+            colorScheme = themeColors,
+            typography = Typography,
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
